80e882c6e418b15f9e6c7188f6472759476610fc
joaoVitorLeal <joaoleal98@outlook.com>
2025-05-05 22:59:43 -0300

    initial commit

---
+++ workshop-api/src/main/java/io/github/joaoVitorLeal/workshop_api/WorkshopApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.joaoVitorLeal.workshop_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopApiApplication.class, args);
+	}
+
+}
---
+++ workshop-api/src/test/java/io/github/joaoVitorLeal/workshop_api/WorkshopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.joaoVitorLeal.workshop_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
